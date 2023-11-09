3c68c3c65c2e8428720f88f179be3639f0584598
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-09 22:48:05 +0900

    :sparkles:feat: ポップアップで今開いているタブの情報を取得する

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,47 +1,21 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import logo from "./assets/logo.svg";
 
 function Popup() {
-	const [count, setCount] = useState(0);
+	const [url, setUrl] = useState('');
+
+	const handleOnClick = () => {
+		chrome.tabs.query({active: true, currentWindow: true}, (tab) => {
+			console.log(tab);
+			console.log(tab[0].title);
+			setUrl(tab[0].url);
+		});
+	};
 
 	return (
 		<div className="App" style={{ height: 300, width: 300 }}>
-			<header className="App-header">
-				<img
-					src={chrome.runtime.getURL(logo)}
-					className="App-logo"
-					alt="logo"
-				/>
-				<p>Hello Vite + React!</p>
-				<p>
-					<button type="button" onClick={() => setCount((count) => count + 1)}>
-						count is: {count}
-					</button>
-				</p>
-				<p>
-					Edit <code>App.tsx</code> and save to test HMR updates.
-				</p>
-				<p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-					{" | "}
-					<a
-						className="App-link"
-						href="https://vitejs.dev/guide/features.html"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Vite Docs
-					</a>
-				</p>
-			</header>
+			<button onClick={handleOnClick}>登録</button>
+			<div>{url}</div>
 		</div>
 	);
 }
